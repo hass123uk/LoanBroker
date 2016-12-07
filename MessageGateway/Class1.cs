5554f6d65f69021dfe67c0e43fc4dad1227304b0
HassanMahmud <Hass123uk@hotmail.com>
2016-12-07 13:25:50 +0100

    Add project files.

---
+++ MessageGateway/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageGateway
+{
+    public class Class1
+    {
+    }
+}
